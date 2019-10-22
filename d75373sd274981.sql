53d1ae04c86acd48eee2c4a734cea3b171c25f7b
DanilGritsenko <49917650+DanilGritsenko@users.noreply.github.com>
2019-10-22 12:43:02 +0300

    Add files via upload

--- d75373sd274981.sql
+++ d75373sd274981.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: d75373.mysql.zonevs.eu
--- Loomise aeg: Okt 18, 2019 kell 12:23 PL
--- Serveri versioon: 10.2.27-MariaDB-log
--- PHP versioon: 7.2.18
+-- Хост: 127.0.0.1
+-- Время создания: Окт 22 2019 г., 11:42
+-- Версия сервера: 10.4.8-MariaDB
+-- Версия PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,151 +19,116 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Andmebaas: `d75373sd274981`
+-- База данных: `d75373sd274981`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `consultations`
+-- Структура таблицы `consultation`
 --
 
-CREATE TABLE `consultations` (
+CREATE TABLE `consultation` (
   `consID` int(11) NOT NULL,
-  `subject` varchar(50) DEFAULT NULL,
-  `constime` datetime DEFAULT NULL,
+  `consname` varchar(100) DEFAULT NULL,
+  `consdesc` varchar(250) DEFAULT NULL,
   `teacher` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `consultation`
+--
+
+INSERT INTO `consultation` (`consID`, `consname`, `consdesc`, `teacher`) VALUES
+(1, 'Testing Cons', 'Consultation for Testing topic, provided by Vasiliy Strelcov!', 8);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `consultationsReg`
+-- Структура таблицы `registrations`
 --
 
-CREATE TABLE `consultationsReg` (
-  `regid` int(11) NOT NULL,
+CREATE TABLE `registrations` (
+  `regID` int(11) NOT NULL,
   `consID` int(11) DEFAULT NULL,
-  `studentID` int(11) DEFAULT NULL,
-  `teacherID` int(11) DEFAULT NULL,
-  `regtime` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `userID` int(11) DEFAULT NULL,
+  `teachID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `registrations`
+--
+
+INSERT INTO `registrations` (`regID`, `consID`, `userID`, `teachID`) VALUES
+(1, 1, 7, 8);
 
 -- --------------------------------------------------------
 
 --
--- Tabeli struktuur tabelile `consultationsRoles`
---
-
-CREATE TABLE `consultationsRoles` (
-  `roleID` int(11) NOT NULL,
-  `roleName` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Andmete tõmmistamine tabelile `consultationsRoles`
---
-
-INSERT INTO `consultationsRoles` (`roleID`, `roleName`) VALUES
-(1, 'Admin'),
-(2, 'Teacher'),
-(3, 'Student');
-
--- --------------------------------------------------------
-
---
--- Tabeli struktuur tabelile `consultationsUser`
---
-
-CREATE TABLE `consultationsUser` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `roleid` int(11) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabeli struktuur tabelile `users`
+-- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT current_timestamp()
+  `created_at` datetime DEFAULT current_timestamp(),
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indeksid tõmmistatud tabelitele
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `name`) VALUES
+(1, 'jadaun', '$2y$10$rHL3bapZkJN7P3cYG8EazefKWRKutRS0ON0yqwfxDgKEcJpqwAHfy', '2019-10-22 09:27:06', ''),
+(7, 'Difictly', '$2y$10$Xw36k2c7EGR4r7WWuaJnceEJKqxR/cv.6O3pnbfQaB5Rv5.XfGbXa', '2019-10-22 09:50:38', 'Danil Gritsenko'),
+(8, 'vasya', '$2y$10$D3bkJmcHD0PdAS5m.oXOm.aWdLMGu.l0K0zyJMu9gkO9aQJbPqYvG', '2019-10-22 10:09:53', 'Vasiliy Strelcov');
+
+--
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indeksid tabelile `consultations`
+-- Индексы таблицы `consultation`
 --
-ALTER TABLE `consultations`
+ALTER TABLE `consultation`
   ADD PRIMARY KEY (`consID`);
 
 --
--- Indeksid tabelile `consultationsReg`
+-- Индексы таблицы `registrations`
 --
-ALTER TABLE `consultationsReg`
-  ADD PRIMARY KEY (`regid`);
+ALTER TABLE `registrations`
+  ADD PRIMARY KEY (`regID`);
 
 --
--- Indeksid tabelile `consultationsRoles`
---
-ALTER TABLE `consultationsRoles`
-  ADD PRIMARY KEY (`roleID`);
-
---
--- Indeksid tabelile `consultationsUser`
---
-ALTER TABLE `consultationsUser`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeksid tabelile `users`
+-- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT tõmmistatud tabelitele
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT tabelile `consultations`
+-- AUTO_INCREMENT для таблицы `consultation`
 --
-ALTER TABLE `consultations`
-  MODIFY `consID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `consultation`
+  MODIFY `consID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT tabelile `consultationsReg`
+-- AUTO_INCREMENT для таблицы `registrations`
 --
-ALTER TABLE `consultationsReg`
-  MODIFY `regid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `registrations`
+  MODIFY `regID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT tabelile `consultationsRoles`
---
-ALTER TABLE `consultationsRoles`
-  MODIFY `roleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT tabelile `consultationsUser`
---
-ALTER TABLE `consultationsUser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT tabelile `users`
+-- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
